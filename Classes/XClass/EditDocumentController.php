d8c12586957a5374de8ef63081d8a730e8a691d4
Benjamin Beck <beck@beck-digitale-medien.de>
2019-01-23 19:26:53 +0000

    [TASK] Initial commit

---
+++ Classes/XClass/EditDocumentController.php
@@ -0,0 +1,35 @@
+<?php
+namespace BDM\BdmSaveandclose\XClass;
+
+/**
+ * Created by PhpStorm.
+ * User: benjamin
+ * Date: 23.01.2019
+ * Time: 20:12
+ */
+
+class EditDocumentController extends \TYPO3\CMS\Backend\Controller\EditDocumentController
+{
+	/**
+	 * Register the save button to the button bar
+	 *
+	 * @param ButtonBar $buttonBar
+	 * @param string $position
+	 * @param int $group
+	 */
+	protected function registerSaveButtonToButtonBar(ButtonBar $buttonBar, string $position, int $group)
+	{
+		parent::registerSaveButtonToButtonBar($buttonBar, $position, $group);
+		$saveButton = $buttonBar->makeInputButton()
+			->setForm('EditDocumentController')
+			->setIcon($this->moduleTemplate->getIconFactory()->getIcon('actions-document-save-close', Icon::SIZE_SMALL))
+			->setName('_saveandclosedok')
+			->setShowLabelText(false)
+			->setTitle($this->getLanguageService()->sL(
+				'LLL:EXT:core/Resources/Private/Language/locallang_core.xlf:rm.saveCloseDoc'
+			))
+			->setValue('1');
+
+		$buttonBar->addButton($saveButton, $position, $group);
+	}
+}
---
+++ ext_emconf.php
@@ -0,0 +1,46 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "BDM.BdmThemeBootstrap".
+ *
+ * Auto generated 30-01-2016 18:03
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Save and close button',
+	'description' => 'Add a save and close button',
+	'category' => 'plugin',
+	'version' => '9.5.1',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 1,
+	'lockType' => '',
+	'author' => '',
+	'author_email' => '',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '9.5.0-9.5.99'
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:0:{}',
+	'suggests' => array(
+	),
+);
